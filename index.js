995227a48f17979d587cc1639c38fe0491d80871
JasperCherry <kacper.wisnicki@gmail.com>
2016-11-10 12:22:50 +0000

    app works
    works on heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,5 +15,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function(){
-  console.log('listening on *:5000');
+  console.log('listening on *:'+port);
 });
